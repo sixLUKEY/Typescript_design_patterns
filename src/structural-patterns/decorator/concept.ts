a6cbe3a9900f0989f131ece08406d29106841b43
sixLUKEY <marcoslukerojo@gmail.com>
2024-04-09 14:11:43 +0200

    Decorator section completed

---
+++ src/structural-patterns/decorator/concept.ts
@@ -0,0 +1,35 @@
+// Decorator Concept Sample Code
+
+interface IComponent {
+  method(): string;
+}
+
+class Component implements IComponent {
+  method(): string {
+    return "Component Method";
+  }
+}
+
+class Decorator implements IComponent {
+  protected object: IComponent;
+
+  constructor(object: IComponent) {
+    this.object = object;
+  }
+
+  method(): string {
+    return `Decorator Method(${this.object.method()})`;
+  }
+}
+
+// Client side
+const COMPONENT = new Component();
+console.log(COMPONENT.method());
+
+// The component can be decorated
+const Decorated = new Decorator(COMPONENT);
+console.log(Decorated.method());
+
+// The decorated component can be decorated again
+const Decorated2 = new Decorator(Decorated);
+console.log(Decorated2.method());
